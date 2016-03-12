eabd3069a38485732803bac1e0d669bc165763b9
Ozzie Neher <oz.neher@gmail.com>
2016-03-12 16:46:26 -0600

    Added some documentation for search

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -18,11 +18,15 @@ class PagesController extends Controller
     }
 
     public function getSearch(Request $request) {
+      // Store the search so we can send it with the view
       $search = $request->q;
 
+      // Check if the search directly matches a tag
       if (Tag::whereName($search)->exists()) {
+        // If a tag is found we'll just show all projects with that tag
         $projects = Tag::whereName($search)->first()->projects->reverse();
       } else {
+        // Otherwise find the matching projects by name or description
         $projects = Project::where('name', 'LIKE', '%' . $search . '%')
                             ->orWhere('description', 'LIKE', '%' . $search . '%')
                             ->orderBy('created_at', 'DESC')
